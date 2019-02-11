db7e992636750b7a0a81e5bd3440d9f497436278
sunsunnp <sunsun.np@gmail.com>
2019-02-11 13:24:34 +0700

    Fit syntax Controller Users #3

--- application/controllers/Users/Users.php
+++ application/controllers/Users/Users.php
@@ -3,19 +3,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Users extends CI_Controller
 {
-	public function __construct()
+    public function __construct()
 	{
-		parent::__construct();
-		$this->load->helper('url');
+	    parent::__construct();
+	    $this->load->helper('url');
 	}
 
-	public function student()
+    public function student()
 	{
-		$this->load->view('users/student');
-  }
+        $this->load->view('users/student');
+    }
     
-  public function teacher()
-	{
-		$this->load->view('users/teacher');
-  }  
+    public function teacher()
+    {
+	    $this->load->view('users/teacher');
+    }  
 }
