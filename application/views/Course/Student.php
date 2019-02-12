2361505073277725484005b4615db1a14a59898f
Wichuta18 <kimwweir9@gmail.com>
2019-02-12 13:37:07 +0700

    Edit Student

--- application/views/Course/Student.php
+++ application/views/Course/Student.php
@@ -17,7 +17,7 @@ form {
   border: 1px solid #f1f1f1 ; 
   margin:50px 0px; 
   padding:0px; align:center; 
-  width: 80%;  
+  width: 90%;  
   background-color: #ffffff;
   border-radius: 20px; }
 
@@ -78,52 +78,77 @@ button:hover {
 
 <div class="container">
 
-  <form class="form-horizontal" action="/action_page.php" >
-   <h2 align="center" class="mt-4 mb-3">ป้อนข้อมูลรายวิชาที่ไม่ผ่าน</h2><br>
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="Idnumber">รหัสนักศึกษา:</label>
-      <div class="col-sm-3">
+  <form class="form-inline" action="/action_page.php">
+    <h2 align="center" class="mt-4 mb-3">ป้อนข้อมูลรายวิชาที่ไม่ผ่าน</h2><br><hr>
+    <div class="form-group col-md-12">
+      <div class="col-md-6">
+        <label for="email">รหัสนักศึกษา:</label>
+        <input type="Idnumber" class="form-control" id="Idnumber" placeholder=" " name="Idnumber">
+      </div>
+      <div class="col-md-6">
+        <label for="idcourse">รหัสวิชา:</label>
+        <input type="idcourse" class="form-control" id="idcourse" placeholder="" name="idcourse">
+      </div>
+    </div>
+    <div class="form-group col-md-12">
+      <div class="col-md-6">
+        <label for="year">ปีการศึกษา:</label>
+        <input type="Idnumber" class="form-control" id="Idnumber" placeholder=" " name="Idnumber">
+      </div>
+      <div class="col-md-6">
+      <label  for="pwd">ภาคการศึกษา:</label>
+        <select>
+          <option class="dropdown-item" value="volvo">1</option>
+          <option class="dropdown-item" value="saab">2</option>
+          <option class="dropdown-item" value="opel">3</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-group col-md-12">
+      <div class="col-md-6">
+        <label for="pwd">เกรดที่ได้รับ:</label>
+        <select  name="grade">
+          <option value="volvo">W</option>
+          <option value="saab">F</option>
+          <option value="opel">D</option>
+          <option value="opel">D+</option>
+        </select>
+      </div>
+    </div>
+ 
+
+      <div class="form-group">        
+        <div class="col-sm-offset-5 col-sm-10">
+          <button type="submit" class="btn btn-primary">บันทึกข้อมูล</button>
+        </div>
+      </div><hr>
+  </div> 
+  </form>
+</div>
+
+</body>
+</html>
+     <!-- <div class="col-sm-3">
         <input type="Idnumber" class="form-control" id="Idnumber" placeholder="รหัสนักศึกษา" name="Idnumber">
       </div>
-    </div>
+    </div> -->
     
+    
+   
+    <!-- <form class="form-inline" action="/action_page.php">
     <div class="form-group">
-      <label class="control-label col-sm-2" for="idcourse">รหัสวิชา:</label>
-      <div class="col-sm-3">          
-        <input type="idcourse" class="form-control" id="idcourse" placeholder="รหัสวิชา" name="idcourse">
-      </div>
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
     </div>
-    
-    
     <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+     -->
+    
+    <!-- <div class="form-group">
       <label class="control-label col-sm-2" for="year">ปีการศึกษา:</label>
       <div class="col-sm-3">          
         <input type="year" class="form-control" id="year" placeholder="ปีการศึกษา" name="year">
       </div>
-    </div>
-    <label class="control-label col-sm-2" for="pwd">ภาคการศึกษา:</label>
-    <select>
-      <option class="dropdown-item" value="volvo">1</option>
-      <option class="dropdown-item" value="saab">2</option>
-      <option class="dropdown-item" value="opel">3</option>
-    </select>
-<br>
-<br>
-
-    <label class="control-label col-sm-2" for="pwd">เกรดที่ได้รับ:</label>
-    <select  name="grade">
-    <option value="volvo">W</option>
-    <option value="saab">F</option>
-    <option value="opel">D</option>
-    <option value="opel">D+</option>
-    </select>
-    <div class="form-group">        
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-primary">บันทึกข้อมูล</button>
-      </div>
-    </div>
-  
-</form>
-
-</body>
-</html>
+    </div> -->
